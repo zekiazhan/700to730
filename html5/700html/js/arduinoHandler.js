272d366488d3f80c5933ca6870210dfd795128e8
jsohn96 <jsohn96@hotmail.com>
2016-02-20 13:29:41 -0800

    Main added js

--- html5/700html/js/arduinoHandler.js
+++ html5/700html/js/arduinoHandler.js
@@ -1,4 +1,3 @@
-          var text;// variable for the text div you'll create
           var socket = new WebSocket("ws://localhost:8081");
          
           function setup() {
@@ -14,9 +13,8 @@
 
           function showData(result) {
               var res = result.data.toString();
-              // console.log(res);
+              //split the three dataInput to array
               var resArray = res.split("/");
-              // convert result to an integer
 
               //all variable range is form 0-1023
               //1st knob - G
@@ -24,16 +22,9 @@
               //2nd camera - W
               realityView = int(resArray[1]);
               //3rd camera for audio
-              audioVolume = int(resArray[2]);
+              audioView = int(resArray[2]);
               // send a byte to get the Arduino to send new data
               socket.send('700 to 730');
 
-              doKeyDown(pointOfView);
-                // e.which = 103; // # Some key code value
-                // $("input").trigger(e);
-                // videos[0].show();
-                // videos[1].show();
-                // temOpacity -= 0.1;
-                // videos[0].css("opacity",temOpacity);
-                // videos[1].css("opacity",1-temOpacity);
+              // doKeyDown(pointOfView); //for testing
             }
